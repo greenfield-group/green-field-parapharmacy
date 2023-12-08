06b5b384b3943762264fbb08e3263b3ca06feaaf
aziza0412 <azizabenhalima@live.fr>
2023-12-08 18:30:54 +0100

    auth done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthContextProvider } from './context/authContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <React.StrictMode>
+    <AuthContextProvider>
     <Router>
     <App />
     </Router>
-  </React.StrictMode>,
+     </AuthContextProvider>
+  </React.StrictMode>
 
 )
