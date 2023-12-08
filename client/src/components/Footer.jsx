c08d461f231458195277b5e5a0a67b5369eae374
nessrineatig <nesrineatig14@gmail.com>
2023-12-08 21:20:58 +0100

    some modification to the App , I added the footer and contact

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white text-center py-4">
+      <p>&copy; 2023 Medicare. All rights reserved.</p>
+      <p className="italic">"Your healthcare is our journey."</p>
+    </footer>
+  );
+};
+
+export default Footer;
