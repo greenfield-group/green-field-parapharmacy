848ddd91757974a9e52e093da00fabf18c3ba427
aziza0412 <azizabenhalima@live.fr>
2023-12-10 22:25:56 +0100

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,30 +8,42 @@ const Navbar = () => {
   return (
     <div>
 
-    <nav className="bg-black" >
-      <div className="space-y-1 px-2 pb-3 pt-2">
-        <NavLink to="/" className="text-white text-xl font-semibold" >
-         MediCare
-        </NavLink>
-         <div className="flex space-x-4">
-          <NavLink to="/" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"  >
-            Home
+      <nav className="bg-black" >
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          <NavLink to="/" className="text-white text-xl font-semibold" >
+            MediCare
           </NavLink>
-           {(!currentUser) &&<NavLink to="/signup" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" >
-            Sign up
-          </NavLink>}
-          {(!currentUser)&&<NavLink to="/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" >
-            Contact
-          </NavLink>}
-          {currentUser&&<span onClick={()=>{if (currentUser.admin ===0){navigate(`/user/${currentUser.iduser}`)}
-           else {navigate(`/admin/${currentUser.iduser}`)} }} className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">{currentUser.name}</span>}
+          <div className="flex space-x-4">
+            <NavLink to="/" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"  >
+              Home
+            </NavLink>
+            {(!currentUser) && <NavLink to="/signup" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" >
+              Sign up
+            </NavLink>}
+            {(!currentUser) && <NavLink to="/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" >
+              Contact
+            </NavLink>}
+            {currentUser && <span onClick={() => {
+              if (currentUser.admin === 0) { navigate(`/user/${currentUser.iduser}`) }
+              else { navigate(`/admin/${currentUser.iduser}`) }
+            }} className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">{currentUser.name}</span>}
+            {currentUser && (
+              <NavLink
+                to="/cart/:iduser"
+                className="flex items-center text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+              >
+                <span role="img" aria-label="Shopping Bag" className="mr-2">
+                  🛒
+                </span>
 
-          {(currentUser) ? <span onClick={()=>{logout(currentUser); navigate('/')}} className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Logout</span>
-          :<NavLink to="/signin" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" >
-          Sign in
-          </NavLink>}
+              </NavLink>
+            )}
+            {(currentUser) ? <span onClick={() => { logout(currentUser); navigate('/') }} className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Logout</span>
+              : <NavLink to="/signin" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium" >
+                Sign in
+              </NavLink>}
+          </div>
         </div>
-       </div> 
       </nav>
     </div>
   );
